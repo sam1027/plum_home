b8ccd4064d0c9bd0a51384e1da61e223f7122293
Seongeun Yu <yse1027@gmail.com>
2023-09-05 14:35:21 +0900

    header style 작업중..

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,31 @@
+import { styled } from "styled-components";
+import oc from 'open-color';
+import { Link } from "react-router-dom";
+
+const HeaderDiv = styled.div`
+    width: 100%;
+    border-bottom: 2px solid ${oc.violet[4]};
+`;
+
+const Logo = styled.span`
+    font-size: 50px;
+    font-weight: 500;
+    color: ${oc.violet[9]};
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+`;
+
 const Header = () => {
     return (
-        <header>
-           <div>
-                <div>
-                    로고
-                </div>
-
-                <nav>
-                    <ul>
-                        <li>메뉴1</li>
-                        <li>메뉴2</li>
-                    </ul>
-                </nav>
-            </div> 
-        </header>
+        <HeaderDiv>
+            <StyledLink to="/">
+                <Logo>
+                    PLUM
+                </Logo>
+            </StyledLink>
+        </HeaderDiv>
     );
 }
  
